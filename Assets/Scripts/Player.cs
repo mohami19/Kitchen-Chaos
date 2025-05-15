44897642959f2c5569da22597097ea045b8ab7bf
Mohammad Mahdi Gholami <gholamimahdi680@gmail.com>
2025-05-15 14:22:23 +0330

    Add Player movement script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float movespeed = 6f;
+    [SerializeField] private float rotation = 10f;
+    private void Update()
+    {
+        Vector2 inputVector = new();
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1;
+        }
+
+        inputVector = inputVector.normalized;
+        Vector3 moveDir = new(inputVector.x, 0f, inputVector.y);
+        transform.position += movespeed * Time.deltaTime * moveDir;
+        transform.forward = Vector3.Slerp(transform.forward,moveDir, Time.deltaTime * rotation);
+    }
+}
