dd59fe354766723816fc899e0710e2190d388aea
Mohammad Mahdi Gholami <gholamimahdi680@gmail.com>
2025-07-28 18:22:31 +0330

    Sync players movement across network

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -56,6 +56,8 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
 
     private void Update()
     {
+        if (!IsOwner) return;
+
         HandleMovement();
         HandleInteraction();
     }
